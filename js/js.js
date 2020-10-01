5c08543f05ddf23772cb4f7742de327bd5c7b33d
natalia-usach <58885439+natalia-usach@users.noreply.github.com>
2020-10-01 11:15:13 +0300

    Added all the files
    Practically all

---
+++ js/js.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel();
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+    $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+    $(".item5").click(function(){
+    $("#myCarousel").carousel(4);
+  });
+    $(".item6").click(function(){
+    $("#myCarousel").carousel(5);
+  });
+  // Enable Carousel Controls
+  $(".carousel-control-prev").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
+
+$(document).ready(function(){
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+        $('#back-to-top').fadeIn();
+      } else {
+        $('#back-to-top').fadeOut();
+      }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 400);
+      return false;
+    });
+});
